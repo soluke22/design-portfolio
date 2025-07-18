76053f49b3adbdbf706fe95f2fcbd2b86a60fd31
Solomon Lucas <soluke22@gmail.com>
2025-07-18 15:35:28 -0400

    removed theme button

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Theme Toggle
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme');
-    });
-
     // Smooth Scrolling
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
